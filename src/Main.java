4bdaaf79dac0070a1f136b62f65fd1b8541d5941
BernardoAndrade <bn.andrade@campus.fct.unl.pt>
2026-09-20 14:44:33 +0100

    Constantes das mensagens adicionadas

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,9 @@ public class Main {
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
+    public static final String PHONE_NOT_EXIST = "Phone number does not exist.";
+    public static final String SAME_NUMBER = "There are contacts that share phone numbers.";
+    public static final String DIF_NUMBER = "All contacts have different phone numbers.";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
